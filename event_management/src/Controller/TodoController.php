b8d7d8c72fc8ec34f4438b881ee4276ce1a513bb
Senimtra <senimtra@hotmail.com>
2021-05-14 14:24:54 +0200

    add create functionality

--- event_management/src/Controller/TodoController.php
+++ event_management/src/Controller/TodoController.php
@@ -2,9 +2,29 @@
 
 namespace App\Controller;
 
+// ### Add Classes to handle the create inputs ###
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+// ### Add IntegerType Component to handle event capacity ###
+
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+// ### Add Request Component
+
+use Symfony\Component\HttpFoundation\Request;
+
+// ### Add Entity Todo ###
+
+use App\Entity\Todo;
 
 class TodoController extends AbstractController
 {
@@ -12,13 +32,89 @@ class TodoController extends AbstractController
     public function index(): Response
     {
         // ### fetch all records from the database ###
+
         $todos = $this->getDoctrine()->getRepository('App:Todo')->findAll();
         return $this->render('todo/index.html.twig', array('todos' => $todos));
     }
     #[Route('/create', name: 'todo_create')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        return $this->render('todo/create.html.twig');
+
+        // ### Creating a new object from Todo ###
+
+        $todo = new Todo;
+
+        // ### Builing the form by using the createFormBuilder-function ###
+
+        $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('image', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('capacity', IntegerType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('date', DateTimeType::class, array('attr' => array('style' => 'margin-bottom:15px')))
+            ->add('email', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('phone', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('address', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('url', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('type', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label' => 'Create Todo', 'attr' => array('class' => 'btn-primary', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+        $form->handleRequest($request);
+
+        // ### Form-submit validation ###
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // ### Getting the data from the inputs ###
+
+            $name = $form['name']->getData();
+            $image = $form['image']->getData();
+            $description = $form['description']->getData();
+            $capacity = $form['capacity']->getData();
+            $date = $form['date']->getData();
+            $email = $form['email']->getData();
+            $phone = $form['phone']->getData();
+            $address = $form['address']->getData();
+            $url = $form['url']->getData();
+            $type = $form['type']->getData();
+
+            // ### Setting the data in the Entity ###
+
+            $todo->setName($name);
+            $todo->setImage($image);
+            $todo->setDescription($description);
+            $todo->setCapacity($capacity);
+            $todo->setDate($date);
+            $todo->setEmail($email);
+            $todo->setPhone($phone);
+            $todo->setAddress($address);
+            $todo->setUrl($url);
+            $todo->setType($type);
+
+            // ### Preparing the database query ###
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+
+            // ### Performing the database query ###
+
+            $em->flush();
+
+            // ### Print notice ###
+
+            $this->addFlash(
+                'notice',
+                'Todo Added'
+            );
+
+            // ### Redirect to Home (index) ###
+
+            return $this->redirectToRoute('todo');
+        }
+
+        /* now to make the form we will add this line form->createView() and now you can see the form in create.html.twig file  */
+
+        return $this->render('todo/create.html.twig', array('form' => $form->createView()));
     }
     #[Route('/edit/{id}', name: 'todo_edit')]
     public function edit(): Response
